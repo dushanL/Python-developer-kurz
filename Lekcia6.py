d300e2dd7dbb6e003fcdab1cf4b0fbee3ca415bb
dusan.lichner <dusan.lichner@mt.coop.sk>
2025-08-19 19:55:59 +0200

    Add file Lekcia6.py

--- Lekcia6.py
+++ Lekcia6.py
@@ -1,3 +1,11 @@
 # Lekcia6
 # vytvoril som si github nasledne som si pouzil clone, kedze pouzivam 2FA bolo treba spravit clone pomoocu
 # git clone https://dushanL:TOKEN_VYGENEROVANY_NA_GITHUB@github.com/dushanL/Python-developer-kurz.git
+
+
+# zalozenie git repozitara lokalne git init
+# pridanie suborov do staging area git add .
+# vytvorenie commitu git commit -m "popis commitu"
+# vratenie sa k zmene git reset HEAD^
+# pre subory ktore nechcem verziovat, treba vytvorit subor .gitignore so zoznamom suborov/adresarov. Ak som uz pridal subor do stage a potom ho pridam do .ignore, musim pouzit git reset
+# git reset vymaze subory so stage area
