2fc110a0534322ef63d129c1dea01418b3f37a1a
dusan.lichner <dusan.lichner@mt.coop.sk>
2025-08-19 19:47:38 +0200

    Add file Lekcia6.py

--- Lekcia6.py
+++ Lekcia6.py
@@ -1,1 +1,3 @@
 # Lekcia6
+# vytvoril som si github nasledne som si pouzil clone, kedze pouzivam 2FA bolo treba spravit clone pomoocu
+# git clone https://dushanL:TOKEN_VYGENEROVANY_NA_GITHUB@github.com/dushanL/Python-developer-kurz.git
