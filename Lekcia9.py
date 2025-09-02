8e5594ca646811b292f27d74f9c3a057d5645f83
dusan.lichner <dusan.lichner@mt.coop.sk>
2025-09-02 18:51:23 +0200

    clear whitespaces

--- Lekcia9.py
+++ Lekcia9.py
@@ -1,19 +1,10 @@
-
 ###############DOPLNENY TEXT Z TESTU
 #vytvorenie novej branche git checkout -b nazov_branche
-
 #medzi branche sa prepinam git checkout nazov_branche
 #zoznam branche git branch
-
-
 #doplnim nieco do branche test git push --set-upstream origin test
-
 #git push musim pouzit v tvare
 ###############DOPLNENY TEXT Z TESTU
-
-          
 ###############DOPLNENY TEXT Z MAIN
-
-  #v main branche mam iba tento text
-  
-###############DOPLNENY TEXT Z MAIN 
+#v main branche mam iba tento text
+###############DOPLNENY TEXT Z MAIN
