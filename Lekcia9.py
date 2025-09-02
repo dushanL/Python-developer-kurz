1fc8fb92d81946cb93ccfbc918ed8fdacafe090f
dusan.lichner <dusan.lichner@mt.coop.sk>
2025-09-02 18:26:35 +0200

    doplnenie textov

---
+++ Lekcia9.py
@@ -0,0 +1,9 @@
+#vytvorenie novej branche git checkout -b nazov_branche
+
+#medzi branche sa prepinam git checkout nazov_branche
+#zoznam branche git branch
+
+
+#doplnim nieco do branche test git push --set-upstream origin test
+
+#git push musim pouzit v tvare
